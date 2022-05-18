d0c2f0c055bb1e907c8867cb4179468f35ec9ac0
ligervision <vigerlizion@gmail.com>
2022-05-18 08:27:54 -0500

    first commit

---
+++ geometry.py
@@ -0,0 +1,9 @@
+import math
+
+def circle_area(radius):
+    area = math.pi * (radius **2)
+    return area
+
+def hypotenuse(side1, side2):
+    hypotenuse = ((side1 **2) + (side2 **2)) ** (1/2)
+    return hypotenuse
